1856164e78cbb9b577e93123893ae2cd09ebab17
paul chesneau <pchesneau3103@gmail.com>
2023-03-17 22:10:34 +0100

    patch and Table

---
+++ tp/structurededonnees/Table.java
@@ -0,0 +1,35 @@
+package tp.structurededonnees;
+
+import java.util.HashMap;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Table<E> {
+
+    private E[] data;
+    private Table(E ...args){
+        this.data = args;
+    }
+
+    public static Table of(Object ...args){
+        for (Object elem:
+             args) {
+            if (elem == null){
+                throw new NullPointerException();
+            }
+        }
+        return new Table(args);
+    }
+
+    public int size(){
+        return data.length;
+    }
+
+    public <T,R> HashMap<T,R[]> groupBy(Comparable<T> value, Function<T,R> pfunc){
+            HashMap<T,R[]> ret = new HashMap<>();
+
+            return ret;
+    }
+
+
+}
